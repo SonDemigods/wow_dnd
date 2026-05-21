8546dda5ccbf202c3cad10fe5dffbc686a89974f
demigods <a13752365641@126.com>
2026-05-21 11:56:12 +0800

    clear:清空老版本的程序。

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,7 @@
 <template>
-  <div id="app">
-    <StartScreen v-if="gameStore.gameState === 'start'" />
-    <CharacterCreation v-else-if="gameStore.gameState === 'creation'" />
-    <GameInterface v-else-if="gameStore.gameState === 'playing'" />
-    <Notification />
-  </div>
+  <div id="app"></div>
 </template>
 
-<script setup lang="ts">
-/**
- * @fileoverview 游戏应用根组件
- * @description 魔兽世界风格RPG游戏的主应用入口，管理游戏状态的切换和主要页面渲染
- * @module App
- */
+<script setup lang="ts"></script>
 
-import { onMounted, computed } from 'vue'
-import { useGameStore } from './stores/game'
-import StartScreen from './components/StartScreen.vue'
-import CharacterCreation from './components/CharacterCreation.vue'
-import GameInterface from './components/GameInterface.vue'
-import Notification from './components/Notification.vue'
-
-/** 游戏状态管理Store实例 */
-const gameStore = useGameStore()
-
-/**
-   * 组件挂载时检查是否有保存的游戏进度
-   * 如果有有效存档且已创建角色，直接进入游戏
-   */
-  onMounted(() => {
-    const hasSavedGame = localStorage.getItem('wow_character_info')
-    if (hasSavedGame) {
-      try {
-        const info = JSON.parse(hasSavedGame)
-        if (info.race && info.charClass) {
-          gameStore.setGameState('playing')
-        }
-      } catch (e) {
-        console.error('Failed to parse saved character info:', e)
-      }
-    }
-  })
-</script>
-
-<style scoped lang="less">
-#app {
-  min-height: 100vh;
-}
-</style>
+<style scoped lang="less"></style>
